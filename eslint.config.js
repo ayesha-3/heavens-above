eac2d8fea46f5e860079b324bb6c084590391508
xy <xyz>
2025-10-31 13:28:40 +0500

    Updated eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,37 +1,23 @@
+// eslint.config.js
 import js from '@eslint/js';
-import prettier from 'eslint-config-prettier';
-import jest from 'eslint-plugin-jest';
+import globals from 'globals';
 
 export default [
-  js.configs.recommended,
-  prettier,
   {
-    files: ['**/*.js'],
-    languageOptions: {
-      ecmaVersion: 2022,
-      sourceType: 'module',
-      globals: {
-        console: 'readonly',
-        process: 'readonly',
-        __dirname: 'readonly',
-        __filename: 'readonly',
-      },
-    },
-    rules: {
-      'no-console': 'off',
-      'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
-    },
+    ignores: ["node_modules", "dist", "docs/scripts/prettify/**"],
   },
+  js.configs.recommended,
   {
-    files: ['**/*.test.js', '**/*.spec.js'],
-    plugins: { jest },
     languageOptions: {
-      globals: {
-        ...jest.environments.globals.globals,
-      },
+      globals: globals.node,
+      ecmaVersion: "latest",
+      sourceType: "module",
     },
     rules: {
-      ...jest.configs.recommended.rules,
+      "no-unused-vars": "warn",
+      "no-undef": "error",
+      "no-redeclare": "error",
+      "no-useless-escape": "warn",
     },
   },
 ];
