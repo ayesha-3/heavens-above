65e00f555150927b39964bc283fd626ded33efd6
xy <xyz>
2025-10-31 11:27:50 +0500

    edit run.js

--- run.js
+++ run.js
@@ -1,5 +1,5 @@
-const satellite = require("./src/satellite");
-const iridium = require("./src/iridium");
+import satellite from "./src/satellite.js";
+import iridium from "./src/iridium";
 
 var location = [39.9042, 116.4074, "%E5%8C%97%E4%BA%AC%E5%B8%82", 52, "ChST"];
 //COOKIE需要先通过浏览器调到中文
